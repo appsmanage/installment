9cf9683f7d44d933102a45c9499d7e7e458f9e7c
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-06-16 23:46:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var key = params.get('key');
 $(function () {
     // 送信
     $('form').submit(function () {
-        var reasons = $('textarea[name="reasons"]').val();
-        // var date = $('input[name="date"]').val();
-        // var tool = $('input[name="tool"]').val();
+        var frequency = $('select[name="frequency"]').val();
+        var BTCamountBF = $('input[name="BTCamountBF"]').val();
+        var ETHamountBF = $('input[name="ETHamountBF"]').val();
+        var XRPamountBF = $('input[name="XRPamountBF"]').val();
         
-        //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\n理由：${reasons}`;
+        var msg = `${key}\n積立頻度：${frequency}\n(bitFlyer)\nBTC：${BTCamoutBF}\nETH：${ETHamoutBF}\nXRP：${XRPamoutBF}`;
         sendText(msg);
 
         return false;
