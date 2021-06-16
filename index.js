7a70c665ab9f65d479fdca153e4be7a3cbc64311
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-06-17 00:10:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var frequency = $('[name="frequency"] option:selected').val();
-        var BTCamountBF = $('input[name="BTCamountBF"]').val();
-        var ETHamountBF = $('input[name="ETHamountBF"]').val();
-        var XRPamountBF = $('input[name="XRPamountBF"]').val();
+        var BTCBF = $('input[name="BTCamountBF"]').val();
+        var ETHBF = $('input[name="ETHamountBF"]').val();
+        var XRPBF = $('input[name="XRPamountBF"]').val();
         
-        var msg = `${key}\n積立頻度：${frequency}\n(bitFlyer)\nBTC：${BTCamountBF}\nETH：${ETHamountBF}\nXRP：${XRPamountBF}`;
+        var msg = `${key}\n積立頻度：${frequency}\n(bitFlyer)\nBTC：${BTCBF}\nETH：${ETHBF}\nXRP：${XRPBF}`;
         sendText(msg);
 
         return false;
