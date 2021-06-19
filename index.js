5e4d3edeffe6246b74fa4fe146f51b3a394ce429
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-06-19 09:50:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ $(function () {
         var btcbf = $('input[name="BTCamountBF"]').val();
         var ethbf = $('input[name="ETHamountBF"]').val();
         var xrpbf = $('input[name="XRPamountBF"]').val();
-        var apikye = $('input[name="apikey"]').val();
+        var apikey = $('input[name="apikey"]').val();
         var apisecret = $('input[name="apisecret"]').val();
         
-        var msg = `${key}\nお名前：${username}\nEmail：${email}\n積立タイミング：${frequency}\n(bitFlyer)\nBTC：${btcbf}円\nETH：${ethbf}円\nXRP：${xrpbf}円\nAPI KYE：********\nAPI SECRET：********`;
+        var msg = `${key}\nお名前：${username}\nEmail：${email}\n積立タイミング：${frequency}\n(bitFlyer)\nBTC：${btcbf}円\nETH：${ethbf}円\nXRP：${xrpbf}円\nAPI KYE：${apikey}\nAPI SECRET：${apisecret}`;
         sendText(msg,apikye,apisecret);
 
         return false;
