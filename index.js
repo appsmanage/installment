fd4c3706b64d8f8eddfeda551d6e0c6869a727c8
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-06-29 23:08:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 $(function () {
     // 送信
     $('form').submit(function () {
-        var username = $('input[name="username"]').val();
-        var email = $('input[name="email"]').val();
         var frequency = $('[name="frequency"] option:selected').val();
         var btcbf = $('input[name="BTCamountBF"]').val();
         var ethbf = $('input[name="ETHamountBF"]').val();
         var xrpbf = $('input[name="XRPamountBF"]').val();
         var xlmbf = $('input[name="XLMamountBF"]').val();
         
-        var msg = `積立申請フォーム\nお名前：${username}\nEmail：${email}\n積立タイミング：${frequency}\nBTC：${btcbf}円\nETH：${ethbf}円\nXRP：${xrpbf}円\nXLM：${xlmbf}円`;
+        var msg = `積立申請フォーム\n積立タイミング：${frequency}\nBTC：${btcbf}円\nETH：${ethbf}円\nXRP：${xrpbf}円\nXLM：${xlmbf}円`;
         sendText(msg,apikey,apisecret);
 
         return false;
